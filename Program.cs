214a4ec1e8ea54bbcce90311ede65ff56b6da376
Theodor Nordkvist <teddin2@outlook.com>
2020-12-29 18:20:17 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace RGBtoHEX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program to convert RGB to Hex values\n");
+            Console.Write("\nType RGB value in a format like this: rgb(255, 128 , 64) ");
+            string colorRGB = Console.ReadLine();
+
+            string[] hexPreparation = colorRGB.Split(',');
+            string[] hexPreparation2 = new string[hexPreparation.Length];
+
+            for (int i = 0; i < hexPreparation.Length; i++)
+            {
+                hexPreparation2[i] = hexPreparation[i].Trim('r', 'g', 'b', '(', ',', ' ', ')');
+
+            }
+
+            int red = int.Parse(hexPreparation2[0]);
+            string redAsHex = ConvertIntToHex(red);
+            int green = int.Parse(hexPreparation2[1]);
+            string greenAsHex = ConvertIntToHex(green);
+            int blue = int.Parse(hexPreparation2[2]);
+            string blueAsHex = ConvertIntToHex(blue);
+            Console.WriteLine($"\n\n#{redAsHex}{greenAsHex}{blueAsHex}\n");
+
+        }
+
+        private static string ConvertIntToHex(int colorValue)
+        {
+            int numberOf16s = colorValue / 16;
+            int remainder = colorValue % 16;
+            // Calculate the hex values for each
+            string hexValue = "";
+
+            if (numberOf16s >= 10)
+            {
+                hexValue += ConvertBetween10And15ToHex(numberOf16s);
+            }
+            else
+            {
+                hexValue += numberOf16s.ToString();
+            }
+
+            if (remainder >= 10)
+            {
+                hexValue += ConvertBetween10And15ToHex(remainder);
+            }
+            else
+            {
+                hexValue += remainder.ToString();
+            }
+
+            return hexValue;
+        }
+        private static char ConvertBetween10And15ToHex(int between10and15)
+        {
+            return between10and15 switch
+            {
+                10 => 'a',
+                11 => 'b',
+                12 => 'c',
+                13 => 'd',
+                14 => 'e',
+                15 => 'f',
+                _ => 'z',
+            };
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# Converter for converting RGB color values to Hexadecimal values
+## Just created this for myself. I didn't feel the online converters were that user-friendly
+
+#### Using this to quickly convert from RGB to HEX when creating and styling webpages
+
+## Usage
+
+##### There are many ways to run this application
+##### I like simply typing dotnet run in the console and inserting the RGB values. Something like dotnet run, and when asked for input, type rgb(255, 255, 255).
+##### It's important that the above format is used. It may or may not run correctly, if the format of the RGB value deviates in any way from the format above.
